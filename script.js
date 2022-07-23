6549bf97e663b4237285e559707317c16f620bfc
Abushama Siddique <abushamasiddique175@gmail.com>
2022-07-23 18:51:51 +0530

    Assignment Done

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
+let guess = document.querySelector("#guess");
+let button = document.querySelector("#btn");
+
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+var result = document.querySelector("#num");
+
+button.addEventListener("click",() => {
+    let number = getRandomNumber(-20, 20);
+    let diff = Math.abs(number - guess);
+    if(diff == 0 || (diff <= 2)){
+        result.innerHTML = number +" " + "Hot";
+    }
+    else{
+        result.innerHTML = number +" " + "Cold";
+    }
+});
 
